91800dc0f75e1ba1177db9f147b659fa4f3c32b9
Endy-gamer <gianluca.vendramin10@gmail.com>
2026-07-08 14:27:16 -0400

    h

--- src/main/java/net/mcreator/utility/init/UtilityModScreens.java
+++ src/main/java/net/mcreator/utility/init/UtilityModScreens.java
@@ -8,6 +8,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.api.distmarker.Dist;
 
+import net.mcreator.utility.client.gui.InventoryeditorGUIScreen;
 import net.mcreator.utility.client.gui.EssenctiondistilizerGUIScreen;
 
 @EventBusSubscriber(Dist.CLIENT)
@@ -15,6 +16,7 @@ public class UtilityModScreens {
 	@SubscribeEvent
 	public static void clientLoad(RegisterMenuScreensEvent event) {
 		event.register(UtilityModMenus.ESSENCTIONDISTILIZER_GUI.get(), EssenctiondistilizerGUIScreen::new);
+		event.register(UtilityModMenus.INVENTORYEDITOR_GUI.get(), InventoryeditorGUIScreen::new);
 	}
 
 	public interface ScreenAccessor {
